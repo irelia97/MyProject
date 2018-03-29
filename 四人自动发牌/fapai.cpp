6335227f037871825e1fa58c00150c154a097a69
雪舞 <31470881+irelia97@users.noreply.github.com>
2018-03-29 10:29:43 +0800

    Update fapai.cpp
    Change function name

--- 四人自动发牌/fapai.cpp
+++ 四人自动发牌/fapai.cpp
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-void initCard(int a[], const int n);
-void randCard(int a[], int b[][13]);
-void sortCard(int a[][13]);
-void printCard(const int a[13], int low, int top, const char type[10]);
-void printFinal(const int a[][13]);
+void InitCard(int a[], const int n);
+void RandCard(int a[], int b[][13]);
+void SortCard(int a[][13]);
+void PrintCard(const int a[13], int low, int top, const char type[10]);
+void PrintFinal(const int a[][13]);
 
 int main()
 {
-	int initcards[52] = {0};	//牌堆
-	int cards[4][13]  = {0};	//玩家的牌
+	int initcards[52] = {0}; 
+	int cards[4][13]  = {0};
 	
-	initCard(initcards, 52);
-	randCard(initcards, cards);
-	sortCard(cards);
-	printFinal(cards);
+	InitCard(initcards, 52);
+	RandCard(initcards, cards);
+	SortCard(cards);
+	PrintFinal(cards);
 	
 	system("pause");
 	return 0;
@@ -27,7 +27,7 @@ int main()
 	1  ~ 13 表示方桃牌  14 ~ 26 表示梅桃牌
 	27 ~ 39 表示红花牌  40 ~ 52 表示黑块牌
 */
-void initCard(int a[], const int n)
+void InitCard(int a[], const int n)
 {
 	int i;
 	for(i=0; i<n; ++i)
@@ -36,7 +36,7 @@ void initCard(int a[], const int n)
 
 
 //	分配牌堆，a[]是初始化好的牌堆，b[4][]是待分配的牌堆
-void randCard(int a[], int b[][13])
+void RandCard(int a[], int b[][13])
 {
 	int i, j, index;
 	srand((unsigned)time(NULL));    //  随机种子
@@ -56,7 +56,7 @@ void randCard(int a[], int b[][13])
 }
 
 //  为 4个人的牌从大到小排序，后面遍历输出时即从大到小输出
-void sortCard(int a[][13])
+void SortCard(int a[][13])
 {
 	int i, j, k, tmp;
 	for(i=0; i<4; i++)  //  第 i 个人
@@ -77,7 +77,7 @@ void sortCard(int a[][13])
 }
 
 //  打印一种花色的牌型，low 和 top 是该种花色的下界值和上界值，type[]是花色名字
-void printCard(const int a[13], int low, int top, const char type[10])
+void PrintCard(const int a[13], int low, int top, const char type[10])
 {
 	int i, tmp;
 	printf("%s：", type);
@@ -105,16 +105,16 @@ void printCard(const int a[13], int low, int top, const char type[10])
 }
 
 //  打印四个人的牌型
-void printFinal(const int a[][13])
+void PrintFinal(const int a[][13])
 {
 	int i, j, tmp;
 	for(i=0; i<4; i++)
 	{
 		printf("第%d位玩家的牌型如下：\n", i+1);
 
-		printCard(a[i], 40, 52, "黑桃牌");
-		printCard(a[i], 27, 39, "红桃牌");
-		printCard(a[i], 14, 26, "梅花牌");
-		printCard(a[i],  1, 13, "方块牌");
+		PrintCard(a[i], 40, 52, "黑桃牌");
+		PrintCard(a[i], 27, 39, "红桃牌");
+		PrintCard(a[i], 14, 26, "梅花牌");
+		PrintCard(a[i],  1, 13, "方块牌");
 	}
 }		
